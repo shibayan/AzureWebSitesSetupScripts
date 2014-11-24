1ace7016c353cd8a2c6819004886291ad5aba4da
shibayan <shiba-yan@outlook.com>
2014-11-24 19:11:00 +0900

    命名規約を再度変更

---
+++ runtimes.js
@@ -0,0 +1,68 @@
+var runtimes =
+[
+    {
+        "name": "Go",
+        "versions": [
+            {
+                "version": "1.3.3",
+                "path": "https://raw.githubusercontent.com/shibayan/AzureWebSitesSetupScripts/master/go/1.3.3.bat"
+            },
+            {
+                "version": "1.3.3 x64",
+                "path": "https://raw.githubusercontent.com/shibayan/AzureWebSitesSetupScripts/master/go/1.3.3_x64.bat"
+            }
+        ]
+    },
+    {
+        "name": "Perl",
+        "versions": [
+            {
+                "version": "5.14.4",
+                "path": "https://raw.githubusercontent.com/shibayan/AzureWebSitesSetupScripts/master/perl/5.14.4.bat"
+            },
+            {
+                "version": "5.16.3",
+                "path": "https://raw.githubusercontent.com/shibayan/AzureWebSitesSetupScripts/master/perl/5.16.3.bat"
+            },
+            {
+                "version": "5.18.4",
+                "path": "https://raw.githubusercontent.com/shibayan/AzureWebSitesSetupScripts/master/perl/5.18.4.bat"
+            },
+            {
+                "version": "5.20.1",
+                "path": "https://raw.githubusercontent.com/shibayan/AzureWebSitesSetupScripts/master/perl/5.20.1.bat"
+            }
+        ]
+    },
+    {
+        "name": "Ruby",
+        "versions": [
+            {
+                "version": "1.8.3",
+                "path": "https://raw.githubusercontent.com/shibayan/AzureWebSitesSetupScripts/master/ruby/1.8.3.bat"
+            },
+            {
+                "version": "1.9.3",
+                "path": "https://raw.githubusercontent.com/shibayan/AzureWebSitesSetupScripts/master/ruby/1.9.3.bat"
+            },
+            {
+                "version": "2.0.0",
+                "path": "https://raw.githubusercontent.com/shibayan/AzureWebSitesSetupScripts/master/ruby/2.0.0.bat"
+            },
+            {
+                "version": "2.0.0 x64",
+                "path": "https://raw.githubusercontent.com/shibayan/AzureWebSitesSetupScripts/master/ruby/2.0.0_x64.bat"
+            },
+            {
+                "version": "2.1.3",
+                "path": "https://raw.githubusercontent.com/shibayan/AzureWebSitesSetupScripts/master/ruby/2.1.3.bat"
+            },
+            {
+                "version": "2.1.3 x64",
+                "path": "https://raw.githubusercontent.com/shibayan/AzureWebSitesSetupScripts/master/ruby/2.1.3_x64.bat"
+            }
+        ]
+    }
+];
+
+runtimeLoaded(runtimes);
