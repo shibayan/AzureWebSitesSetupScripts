2ce0dabaa8becd23e97e7a88c3fcf6cc21853ea4
shibayan <shiba-yan@outlook.com>
2014-11-30 23:08:02 +0900

    Ruby 2.1.5 を追加

---
+++ ruby/2.1.5.bat
@@ -0,0 +1,27 @@
+set PATH=%PATH%;D:\7zip
+
+cd /d D:\home\site\wwwroot
+
+mkdir bin & cd bin
+
+mkdir ruby & cd ruby
+
+curl -L -O -s http://dl.bintray.com/oneclick/rubyinstaller/ruby-2.1.5-i386-mingw32.7z
+7za x ruby-2.1.5-i386-mingw32.7z > nul
+
+rm -f ruby-2.1.5-i386-mingw32.7z
+
+mv ruby-2.1.5-i386-mingw32 2.1.5 & cd 2.1.5
+
+set PATH=%PATH%;D:\home\site\wwwroot\bin\ruby\2.1.5\bin
+
+mkdir devkit & cd devkit
+
+curl -L -O -s http://cdn.rubyinstaller.org/archives/devkits/DevKit-mingw64-32-4.7.2-20130224-1151-sfx.exe
+7za x DevKit-mingw64-32-4.7.2-20130224-1151-sfx.exe > nul
+
+rm -f DevKit-mingw64-32-4.7.2-20130224-1151-sfx.exe
+
+ruby dk.rb init
+echo - D:\home\site\wwwroot\bin\ruby\2.1.5 >> config.yml
+ruby dk.rb install
